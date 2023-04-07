f1772466600dfeef97cf48945d4d40778fb0572a
sreyaarc <123823639+sreyaarc@users.noreply.github.com>
2023-04-07 21:29:39 +0530

    script.js updated

--- script.js
+++ script.js
@@ -7,10 +7,7 @@ setInterval(() => {
         minRotation = 6*xMins + xSec/10;
         secRotation = 6*xSec;
     
-        document.getElementById('hour').style.transform = `rotate(${hourRotation}deg)`;  // rotate(xdeg)
+        document.getElementById('hour').style.transform = `rotate(${hourRotation}deg)`; 
         document.getElementById('min').style.transform = `rotate(${minRotation}deg)`;
         document.getElementById('sec').style.transform = `rotate(${secRotation}deg)`;
     }, 1000);
-
-// The setInterval() method calls a function at specified intervals (in milliseconds) ie, repeats the assigned work every 1000 milliseconds(1 sec)
-// The setInterval() method continues calling the function until clearInterval() is called, or the window is closed.
